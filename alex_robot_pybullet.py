e29e77fdf5ec3ef34188cc822f104c8fae0f66b0
Keyhan Kouhkiloui <kkouhkil@ed.ac.uk>
2024-07-26 09:52:39 +0100

    alex robot simulation - pybullet

--- alex_robot_pybullet.py
+++ alex_robot_pybullet.py
@@ -14,6 +14,15 @@ p.loadURDF("plane.urdf", [0, 0, 0], [0, 0, 0, 1])
 
 # load assets
 alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240109_Alex_noHands.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240109_Alex_nubHands.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240109_Alex_PsyonicHands.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240109_Alex_SakeHands.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240516_Alex_TestStand_FixedHead.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240614_Alex_TestStand_FixedHead_BarrettDumbbell.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240619_Alex_TestStand_FixedHead_LEFT_Barett_RIGHT_Psyonic.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240619_Alex_TestStand_FixedHead_LEFT_Psyonic_RIGHT_Barett.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240619_Alex_TestStand_FixedHead_NoHands.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
+# alex_robot = p.loadURDF("/home/keyhan/Documents/boardwalk_robotics/alex-robot-models/alex_description/urdf/20240619_Alex_TestStand_FixedHead_PsyonicHands.urdf", [0, 0, 1], [0, 0, 0, 1], useFixedBase = True)
 obj_of_focus = alex_robot
 
 # focus_position, _ = p.getBasePositionAndOrientation(alex_robot)
@@ -44,11 +53,29 @@ right_arm_current_end_eff_pos = []
 left_arm_current_end_eff_ori = []
 right_arm_current_end_eff_ori = []
 
+left_arm_joint_0_idx = 0
+right_arm_joint_0_idx = 0
+
+# joints index finder
+for i in range (num_of_joints):
+    joint_type = p.getJointInfo(alex_robot, i)
+
+    joint_type = list(joint_type)
+    joint_type[1] = joint_type[1].decode("utf-8")
+    joint_type = tuple(joint_type)
+
+    if joint_type[1] == 'LeftShoulderPitch':
+        left_arm_joint_0_idx = i
+
+    if joint_type[1] == 'RightShoulderPitch':
+        right_arm_joint_0_idx = i
+
+
 # joint type and limit
 for i in range (num_of_joints):
     joint_type = p.getJointInfo(alex_robot, i)
 
-    if i >= 6 and i < 13:
+    if i >= left_arm_joint_0_idx and i < left_arm_joint_0_idx + 7:
         left_arm_joint_index_vec.append(i)
         left_arm_current_joint_value_vec.append(0)
         left_arm_current_link_value_vec.append(0)
@@ -58,8 +85,9 @@ for i in range (num_of_joints):
 
         left_arm_joint_upper_limit = joint_type[9]
         left_arm_joint_upper_limit_vec.append(left_arm_joint_upper_limit)
+   
 
-    if i >= 13 and i < 20:    
+    if i >= right_arm_joint_0_idx and i < right_arm_joint_0_idx + 7:    
         right_arm_joint_index_vec.append(i)
         right_arm_current_joint_value_vec.append(0)
         right_arm_current_link_value_vec.append(0)
@@ -71,6 +99,14 @@ for i in range (num_of_joints):
         right_arm_joint_upper_limit_vec.append(right_arm_joint_upper_limit)
 
     print(joint_type)
+
+print("\n")
+
+print(left_arm_joint_0_idx)
+print(right_arm_joint_0_idx)
+
+print(f"\nleft_arm_joint_idx = {left_arm_joint_index_vec}")
+print(f"right_arm_joint_idx = {right_arm_joint_index_vec}")
 
 print("\n")
 
@@ -88,7 +124,7 @@ print("\n")
 left_arm_desired_joints_value = [0] * len(left_arm_joint_lower_limit_vec)
 right_arm_desired_joints_value = [0] * len(right_arm_joint_lower_limit_vec)
 
-for step in range(100):
+for step in range(250):
 
     focus_position, _ = p.getBasePositionAndOrientation(alex_robot)
     p.resetDebugVisualizerCamera(cameraDistance = 2, cameraYaw = 90, cameraPitch = -30, cameraTargetPosition = focus_position)
